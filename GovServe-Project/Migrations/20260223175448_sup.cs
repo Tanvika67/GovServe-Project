8fef4db54b029532eeb1ca0e2ae74f6c9b19f7e5
2472076 <2472076@LTIN461372>
2026-02-23 23:31:41 +0530

    Pushing

---
+++ GovServe-Project/Migrations/20260223175448_sup.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GovServe_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class sup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CaseID",
+                table: "SLARecords",
+                type: "int",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CompletedDate",
+                table: "Case",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AssignedDate",
+                table: "Case",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Case",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SlaHours",
+                table: "Case",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Escalation",
+                columns: table => new
+                {
+                    EscalationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CaseId = table.Column<int>(type: "int", nullable: false),
+                    EscalatedByUserId = table.Column<int>(type: "int", nullable: false),
+                    PreviousOfficerId = table.Column<int>(type: "int", nullable: false),
+                    NewOfficerId = table.Column<int>(type: "int", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EscalationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ResolvedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EscalationLevel = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Escalation", x => x.EscalationId);
+                    table.ForeignKey(
+                        name: "FK_Escalation_Case_CaseId",
+                        column: x => x.CaseId,
+                        principalTable: "Case",
+                        principalColumn: "CaseId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceReports",
+                columns: table => new
+                {
+                    ReportID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Scope = table.Column<int>(type: "int", nullable: false),
+                    GeneratedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceReports", x => x.ReportID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_User_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Application",
+                columns: table => new
+                {
+                    ApplicationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    DepartmnetName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServiceID = table.Column<int>(type: "int", nullable: false),
+                    DepartmentID = table.Column<int>(type: "int", nullable: false),
+                    DepartmnetID = table.Column<int>(type: "int", nullable: false),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmneName = table.Column<int>(type: "int", nullable: false),
+                    SubmittedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ApplicationStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequiredDocumentDocumentID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Application", x => x.ApplicationID);
+                    table.ForeignKey(
+                        name: "FK_Application_Departments_DepartmneName",
+                        column: x => x.DepartmneName,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Application_Departments_DepartmnetID",
+                        column: x => x.DepartmnetID,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Application_RequiredDocuments_RequiredDocumentDocumentID",
+                        column: x => x.RequiredDocumentDocumentID,
+                        principalTable: "RequiredDocuments",
+                        principalColumn: "DocumentID");
+                    table.ForeignKey(
+                        name: "FK_Application_Services_ServiceID",
+                        column: x => x.ServiceID,
+                        principalTable: "Services",
+                        principalColumn: "ServiceID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Application_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notification",
+                columns: table => new
+                {
+                    NotificationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CaseId = table.Column<int>(type: "int", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notification", x => x.NotificationId);
+                    table.ForeignKey(
+                        name: "FK_Notification_Case_CaseId",
+                        column: x => x.CaseId,
+                        principalTable: "Case",
+                        principalColumn: "CaseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Notification_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CitizenDocument",
+                columns: table => new
+                {
+                    CitizenDocumentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationID = table.Column<int>(type: "int", nullable: false),
+                    DocumentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UploadedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    VerificationStatus = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CitizenDocument", x => x.CitizenDocumentID);
+                    table.ForeignKey(
+                        name: "FK_CitizenDocument_Application_ApplicationID",
+                        column: x => x.ApplicationID,
+                        principalTable: "Application",
+                        principalColumn: "ApplicationID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Case_ApplicationId",
+                table: "Case",
+                column: "ApplicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Application_DepartmneName",
+                table: "Application",
+                column: "DepartmneName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Application_DepartmnetID",
+                table: "Application",
+                column: "DepartmnetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Application_RequiredDocumentDocumentID",
+                table: "Application",
+                column: "RequiredDocumentDocumentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Application_ServiceID",
+                table: "Application",
+                column: "ServiceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Application_UserId",
+                table: "Application",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CitizenDocument_ApplicationID",
+                table: "CitizenDocument",
+                column: "ApplicationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Escalation_CaseId",
+                table: "Escalation",
+                column: "CaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notification_CaseId",
+                table: "Notification",
+                column: "CaseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notification_UserId",
+                table: "Notification",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_DepartmentId",
+                table: "User",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Case_Application_ApplicationId",
+                table: "Case",
+                column: "ApplicationId",
+                principalTable: "Application",
+                principalColumn: "ApplicationID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Case_Application_ApplicationId",
+                table: "Case");
+
+            migrationBuilder.DropTable(
+                name: "CitizenDocument");
+
+            migrationBuilder.DropTable(
+                name: "Escalation");
+
+            migrationBuilder.DropTable(
+                name: "Notification");
+
+            migrationBuilder.DropTable(
+                name: "ServiceReports");
+
+            migrationBuilder.DropTable(
+                name: "Application");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Case_ApplicationId",
+                table: "Case");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Case");
+
+            migrationBuilder.DropColumn(
+                name: "SlaHours",
+                table: "Case");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CaseID",
+                table: "SLARecords",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CompletedDate",
+                table: "Case",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AssignedDate",
+                table: "Case",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
