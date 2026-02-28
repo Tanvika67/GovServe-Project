5074eaacd71e1040401040ba43efad7e7df3ed12
2471931 <2471931@LTIN691627>
2026-02-28 18:44:03 +0530

    pushing

--- GovServe-Project/Repository/Interface/IUserRepository.cs
+++ GovServe-Project/Repository/Interface/IUserRepository.cs
@@ -12,5 +12,8 @@ namespace GovServe_Project.Repository.Interface
 		Task UpdateAsync(Users user);
 		Task DeleteAsync(Users user);
 
-	}
+		Task<List<Users>> GetOfficersByDepartmentAsync(int departmentId);
+		Task<int> GetActiveCaseCountByOfficerAsync(int officerId);
+
+    }
 }
